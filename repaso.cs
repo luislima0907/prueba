fa99af0a7635823c86cab62fb3180b5296e3a184
Luis <llimap5@miumg.edu.gt>
2024-02-03 10:50:43 -0600

    mi primer punto de control

---
+++ repaso.cs
@@ -0,0 +1,11 @@
+namespace Sesion1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            console.WriteLine("Hola mundo");
+            console.ReadLine();
+        }
+    }
+}
---
+++ repaso2.cs
@@ -0,0 +1,10 @@
+namespace MiBiblioteca
+{
+    public class MiPrimeraClase
+    {
+        public decimal Sumar(decimal numero1, decimal numero2)
+        {
+            return numero1 + numero2;
+        }
+    }
+}
